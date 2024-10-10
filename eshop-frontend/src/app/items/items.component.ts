7aee8e6a218a2237e7f10ea1029da68c201bbce6
Haris Tse <haris.tsetsekas@gmail.com>
2024-10-10 10:25:22 +0300

    part2

---
+++ eshop-frontend/src/app/items/items.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../item.service';
+import { Item } from '../item';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrl: './items.component.css'
+})
+export class ItemsComponent implements OnInit {
+
+  items: Item[] = [];
+
+  constructor(private itemService: ItemService) { }
+
+  ngOnInit(): void {
+    this.getItems();
+  }
+
+  getItems(): void {
+    this.itemService.getItems()
+      .subscribe(items => {
+        this.items = items;
+      });
+  }
+}
