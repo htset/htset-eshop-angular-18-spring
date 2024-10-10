765e1f2219c5b3fecaf43e149bf1976f7ca4f3ad
Haris Tse <haris.tsetsekas@gmail.com>
2024-10-10 10:54:37 +0300

    part4

--- eshop-frontend/src/app/components/public/items/items.component.ts
+++ eshop-frontend/src/app/components/public/items/items.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemService } from '../../../services/item.service';
 import { StoreService } from '../../../services/store.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { skip } from 'rxjs';
+import { FilterComponent } from '../../shared/filter/filter.component';
 
 @Component({
   selector: 'app-items',
@@ -11,11 +14,19 @@ export class ItemsComponent implements OnInit {
 
   constructor(
     private itemService: ItemService,
-    public storeService: StoreService) { }
+    public storeService: StoreService,
+    private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.storeService.pageSizeChanges$
       .subscribe(newPageSize => {
+        this.storeService.page = 1;
+        this.getItems();
+      });
+
+    this.storeService.filter$
+      .pipe(skip(1))    //skip getting filter at component creation
+      .subscribe(filter => {
         this.storeService.page = 1;
         this.getItems();
       });
@@ -25,7 +36,8 @@ export class ItemsComponent implements OnInit {
 
   getItems(): void {
     this.itemService.getItems(this.storeService.page,
-      this.storeService.pageSize)
+      this.storeService.pageSize,
+      this.storeService.filter)
       .subscribe(itemPayload => {
         this.storeService.items = itemPayload.items;
         this.storeService.count = itemPayload.count;
@@ -41,4 +53,7 @@ export class ItemsComponent implements OnInit {
     this.storeService._pageSizeSubject.next(this.storeService.pageSize);
   }
 
+  openFilter(): void {
+    this.modalService.open(FilterComponent);
+  }
 }
--- eshop-frontend/src/app/services/item.service.ts
+++ eshop-frontend/src/app/services/item.service.ts
@@ -2,43 +2,76 @@ import { Injectable, } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Item } from '../models/item';
 import { ItemPayload } from '../models/itemPayload';
+import { Filter } from '../models/filter';
 
-const mock_items = [
-  {
-    id: 1, name: 'Adidas Stan Smith', price: 90.0,
-    category: 'Shoes', description: ''
-  },
-  {
-    id: 2, name: 'Nike Air Max', price: 110.0,
-    category: 'Shoes', description: ''
-  },
-  {
-    id: 3, name: 'Reebok Sweat Shirt', price: 45.0,
-    category: 'Clothes', description: ''
-  },
-  {
-    id: 4, name: 'Puma T-Shirt', price: 30.0,
-    category: 'Clothes', description: ''
-  },
-];
-
+const mock_items: ItemPayload = {
+  items: [
+    {
+      id: 1, name: 'Adidas Stan Smith',
+      price: 90.0, category: 'Shoes', description: ''
+    },
+    {
+      id: 2, name: 'Nike Air Max',
+      price: 110.0, category: 'Shoes', description: ''
+    },
+    {
+      id: 3, name: 'Reebok Sweat Shirt',
+      price: 45.0, category: 'Clothes', description: ''
+    },
+    {
+      id: 4, name: 'Puma T-Shirt',
+      price: 30.0, category: 'Clothes', description: ''
+    },
+    {
+      id: 5, name: 'Under Armour',
+      price: 130.0, category: 'Shoes', description: ''
+    },
+    {
+      id: 6, name: 'Nike Sweat shirt',
+      price: 65.0, category: 'Clothes', description: ''
+    },
+    {
+      id: 7, name: 'Spalding basketball',
+      price: 43.0, category: 'Gear', description: ''
+    },
+    {
+      id: 8, name: 'Dumbbell 5kg',
+      price: 3.50, category: 'Gear', description: ''
+    },
+    {
+      id: 9, name: 'New Balance',
+      price: 120.0, category: 'Shoes', description: ''
+    }
+  ],
+  count: 8
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  getItems(page: number, pageSize: number): Observable<ItemPayload> {
+  getItems(page: number, pageSize: number, filter: Filter):
+    Observable<ItemPayload> {
+    let filteredItems: Item[] = mock_items.items.filter(item => {
+      return (
+        item.name.indexOf(filter.name) >= 0
+        &&
+        (filter.categories.length == 0
+          || filter.categories.includes(item.category))
+      );
+    }
+    );
+
     let payload: ItemPayload = {
-      items: mock_items.slice((page - 1) * pageSize, page * pageSize),
-      count: mock_items.length
+      items: filteredItems.slice((page - 1) * pageSize, page * pageSize),
+      count: filteredItems.length
     }
     return of(payload);
   }
 
-
   getItem(id: number): Observable<Item> {
-    return of(mock_items[id - 1]);
+    return of(mock_items.items[id - 1]);
   }
 
   constructor() { }
