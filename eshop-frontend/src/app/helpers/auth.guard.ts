9d3c283f6319dafafab5057d966f4c98a056a144
Haris Tse <haris.tsetsekas@gmail.com>
2024-10-16 13:00:17 +0300

    part10

--- eshop-frontend/src/app/helpers/auth.guard.ts
+++ eshop-frontend/src/app/helpers/auth.guard.ts
@@ -2,68 +2,25 @@ import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot }
   from "@angular/router";
 import { StoreService } from "../services/store.service";
-import { AuthenticationService } from "../services/authentication.service";
-import { lastValueFrom } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
   constructor(
     private router: Router,
-    private storeService: StoreService,
-    private authenticationService: AuthenticationService
+    private storeService: StoreService
   ) { }
 
-  canActivate(route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot) {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const currentUser = this.storeService.user;
     if (currentUser && currentUser.role == 'ADMIN') {
-      if (currentUser.token && !this.tokenExpired(currentUser.token))
-        return true;
-
-      if (!this.refreshToken(currentUser?.token || '',
-        currentUser?.refreshToken || '')) {
-        this.router.navigate(['/login'],
-          { queryParams: { returnUrl: state.url } });
-        return false;
-      }
       return true;
     }
     else if (currentUser && currentUser.role == 'CUSTOMER') {
-      if (currentUser.token && !this.tokenExpired(currentUser.token)) {
-        this.router.navigate(['/items']);
-        return true;
-      }
-
-      if (!this.refreshToken(currentUser?.token || '',
-        currentUser?.refreshToken || '')) {
-        this.router.navigate(['/login'],
-          { queryParams: { returnUrl: state.url } });
-        return false;
-      }
+      this.router.navigate(['/items']);
       return true;
     }
 
-    this.router.navigate(['/login'],
-      { queryParams: { returnUrl: state.url } });
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
     return false;
   }
-
-  private tokenExpired(token: string) {
-    const expiry = (JSON.parse(atob(token.split('.')[1]))).exp;
-    return (Math.floor((new Date).getTime() / 1000)) >= expiry;
-  }
-
-  private async refreshToken(token: string,
-    refreshToken: string): Promise<boolean> {
-    try {
-      this.storeService.isRefreshing = true;
-      await lastValueFrom(this.authenticationService
-        .refreshToken(token, refreshToken));
-      this.storeService.isRefreshing = false;
-      return true;
-    }
-    catch (err) {
-      return false;
-    }
-  }
-}
+} 
--- eshop-frontend/src/app/helpers/jwt.interceptor.ts
+++ eshop-frontend/src/app/helpers/jwt.interceptor.ts
@@ -1,50 +1,88 @@
 import { Injectable } from "@angular/core";
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest }
   from "@angular/common/http";
-import { Observable, filter, switchMap, take } from "rxjs";
+import { BehaviorSubject, Observable, catchError, filter, switchMap, take, throwError } from "rxjs";
 import { environment } from "../../environments/environment";
 import { StoreService } from "../services/store.service";
+import { Router } from "@angular/router";
+import { AuthenticationService } from "../services/authentication.service";
+import { User } from "../models/user";
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  constructor(private storeService: StoreService) { }
+
+  private isRefreshing = false;
+  private refreshTokenSubject: BehaviorSubject<string | null>
+    = new BehaviorSubject<string | null>(null);
+
+  constructor(private router: Router,
+    private storeService: StoreService,
+    private authenticationService: AuthenticationService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler):
     Observable<HttpEvent<any>> {
 
-    console.log(this.storeService.isRefreshing);
-    if (this.storeService.isRefreshing == false
-      || request.url.indexOf('refresh') >= 0) {
-      //If we are not refreshing, go ahead with the request
-      const currentUser = this.storeService.user;
-      const isLoggedIn = currentUser && currentUser.token;
-      const isApiUrl = request.url.startsWith(environment.apiUrl);
-      if (isLoggedIn && isApiUrl) {
-        request = request.clone({
-          setHeaders: {
-            Authorization: `Bearer ${currentUser?.token}`
+    const currentUser = this.storeService.user;
+    const isLoggedIn = currentUser && currentUser.token;
+    const isApiUrl = request.url.startsWith(environment.apiUrl);
+    if (isLoggedIn && isApiUrl) {
+
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser?.token}`
+        }
+      });
+
+      return next.handle(request).pipe(
+        catchError(error => {
+          // If 401 Unauthorized error, try to refresh the token
+          if (error.status === 401 && !this.isRefreshing) {
+            return this.handle401Error(request, next);
           }
-        });
-      }
-      return next.handle(request);
+          return throwError(() => error);
+        })
+      );
     }
-    else {
-      //If we are refreshing, wait until we finish it
-      return this.storeService.isRefreshing$.pipe(
-        filter(isRefreshing => !isRefreshing), //Wait until isRefreshing becomes false
-        take(1), //Take only one value after it becomes false
-        switchMap(() => {
-          //If the token was refreshed, update the request with the new token
-          const updatedUser = this.storeService.user;
-          request = request.clone({
+    return next.handle(request);
+  }
+
+  private handle401Error(request: HttpRequest<any>, next: HttpHandler)
+    : Observable<HttpEvent<any>> {
+    const currentUser = this.storeService.user;
+    if (!this.isRefreshing) {
+      this.isRefreshing = true;
+      this.refreshTokenSubject.next(null);
+
+      return this.authenticationService.refreshToken(currentUser?.token || '',
+        currentUser?.refreshToken || '').pipe(
+          switchMap((newUser: User) => {
+            this.isRefreshing = false;
+            this.refreshTokenSubject.next(newUser.token || '');
+            return next.handle(request = request.clone({
+              setHeaders: {
+                Authorization: `Bearer ${newUser?.token}`
+              }
+            }));
+          }),
+          catchError((err) => {
+            this.isRefreshing = false;
+            this.authenticationService.logout(currentUser?.refreshToken || '');
+            return throwError(() => err);
+          })
+        );
+    } else {
+      return this.refreshTokenSubject.pipe(
+        filter(token => token !== null),
+        take(1),
+        switchMap(token => {
+          return next.handle(request = request.clone({
             setHeaders: {
-              Authorization: `Bearer ${updatedUser?.token}`
+              Authorization: `Bearer ${currentUser?.token}`
             }
-          });
-          //Retry the request with the updated token
-          return next.handle(request);
+          }));
         })
       );
     }
   }
 }
+
